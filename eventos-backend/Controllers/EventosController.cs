00650d329753712c3f5a2f1b86c9a67fc25a36d8
Fernando Hara <fernando.hara@segmenta.tec.br>
2023-09-15 18:57:57 -0300

    CRUD completo

--- eventos-backend/Controllers/EventosController.cs
+++ eventos-backend/Controllers/EventosController.cs
@@ -1,4 +1,5 @@
 ﻿using eventos_backend.Data;
+using eventos_backend.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +10,7 @@ namespace eventos_backend.Controllers
     [ApiController]
     public class EventosController : ControllerBase
     {
-        private EventosDataContext _db;
+        private readonly EventosDataContext _db;
 
         public EventosController(EventosDataContext db) 
         {
@@ -22,6 +23,66 @@ namespace eventos_backend.Controllers
             var eventos = _db.Eventos;
 
             return Ok(eventos);
+        }
+
+        [HttpGet("{id}", Name = "GetEvento")]
+        public IActionResult GetEvento(int id)
+        {
+            var evento = _db.Eventos.FirstOrDefault(e => e.Id == id);
+
+            if(evento != null)
+                return Ok(id);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(EventoModel evento)
+        {
+            try
+            {
+                _db.Eventos.Add(evento);
+                await _db.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(EventoModel evento)
+        {
+            try
+            {
+                var oldEvento = _db.Eventos.FirstOrDefault(e => e.Id == evento.Id);
+
+                if(oldEvento != null)
+                {
+                    oldEvento = evento;
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    return BadRequest("Registro não encontrado");
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(EventoModel evento)
+        {
+            _db.Eventos.Remove(evento);
+            await _db.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
